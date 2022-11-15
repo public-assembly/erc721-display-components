830ff099420ece9cc4af6a7b0ad5eea5eb1a19f8
Dain Blodorn Kim <dain@db13.us>
2022-11-15 09:13:19 -0800

    owner grid test

--- examples/nextjs-dapp/src/components/test-components/OwnerGrid.tsx
+++ examples/nextjs-dapp/src/components/test-components/OwnerGrid.tsx
@@ -6,15 +6,15 @@ function NFTCard() {
   const { nft } = useNFTProvider()
   return (
     <div className="w-full">
-      <RawDisplayer data={nft?.nft} />
+      <img src={nft?.rawData?.token?.image?.mediaEncoding?.large} />
+      <RawDisplayer data={nft?.rawData?.token?.image?.mediaEncoding?.large} />
     </div>
   )
 }
 
 export default function OwnerGrid() {
   const { address, displayName, chain } = useAuth()
-  
-  console.log(chain?.id)
+
   return (
     <div className="flex flex-col relative">
       <div className="sticky top-4 z-50 pb-4">
